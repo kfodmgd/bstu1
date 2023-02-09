3b2f334ee719df55d1dc944a3eed502a82377a1c
kfodmgd <94847299+kfodmgd@users.noreply.github.com>
2023-02-09 17:07:15 +0300

    Add files via upload

---
+++ 3сем/OOP/14/14laba/14laba/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+// Процесс — экземпляр программы во время выполнения, независимый объект,
+//которому выделены системные ресурсы (например, процессорное время и память).
+
+// Поток — определенный способ выполнения процесса. Когда один поток изменяет ресурс процесса,
+//это изменение сразу же становится видно другим потокам этого процесса.
+
+// В .NET исполняемые файлы не обслуживаются прямо внутри процесса Windows.
+//Они обслуживаются в отдельном логическом разделе внутри процесса,
+//который называется доменом приложения (Application Domain — AppDomain). 
+//В процессе может содержаться несколько доменов приложений.
+namespace _14laba
+{
+    class Program
+    {
+        //3
+        static void WriteNums(object num)
+        {
+            int n = (int)num;
+            using (StreamWriter sw =new StreamWriter("nums.txt", false, System.Text.Encoding.Default))
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    sw.WriteLine(i);
+                    Console.WriteLine(i);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+
+        //4 odd
+        public static void OddNumbers(object num)
+        {
+            int n = (int)num;
+            using (StreamWriter sw = new StreamWriter("numbersOdd.txt", false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sw.WriteLine(i);
+                        Console.Write($"{i} ");
+                        Thread.Sleep(120);
+                    }
+                }
+            }
+        }
+        //even
+        public static void EvenNumbers(object num)
+        {
+            int n = (int)num;
+            using (StreamWriter sw = new StreamWriter("numbersEven.txt", true, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sw.WriteLine(i);
+                        Console.Write($"{i} ");
+                        Thread.Sleep(120);
+                    }
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //1 process
+            Process[] proc = Process.GetProcesses();
+            foreach(Process p in proc)
+            {
+                try
+                {
+                    Console.WriteLine("Id процесса: " + p.Id.ToString());
+                    Console.WriteLine("Name процесса: " + p.ProcessName);
+                    Console.WriteLine("Prioritet процесса: " + p.BasePriority.ToString());
+                    Console.WriteLine("Власс приоритета процесса: " + p.PriorityClass.ToString());
+                    Console.WriteLine("Время запуска процесса: " + p.StartTime.ToString());
+                    Console.WriteLine("Время использования процессором: " + p.TotalProcessorTime.ToString());
+                    if(p.StartTime.ToString()!=null)
+                        Console.WriteLine("Состояние: запущен");
+                    Console.WriteLine();
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+
+
+            //2 domain
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine($"Имя домена: {domain.FriendlyName}");
+            Console.WriteLine($"Базовая директория: {domain.BaseDirectory}");
+            Console.WriteLine($"Информация: {domain.SetupInformation}");
+            Console.WriteLine();
+            Assembly buf = null;
+            foreach (Assembly x in domain.GetAssemblies())
+            {
+                Console.WriteLine(x.GetName().Name);
+            }
+            //AppDomain myNewDomain = AppDomain.CreateDomain("Мой новый домен");
+            //Assembly buf2 = myNewDomain.Load(buf.GetName());---загрузка
+            //AppDomain.Unload(myNewDomain);---выгрузка
+            //Console.WriteLine(buf2.ToString());
+
+
+            //3
+            int n;
+            bool check;
+            Console.WriteLine("введите n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Thread NumberThread = new Thread(new ParameterizedThreadStart(WriteNums));//создание потока
+            Thread.Sleep(500);
+            NumberThread.Start(n);//запуск
+            Thread.Sleep(500);
+            Console.WriteLine("\n");
+            Console.WriteLine("Приоритет:   " + NumberThread.Priority);
+            Thread.Sleep(100);
+            Console.WriteLine("Имя потока:  " + NumberThread.Name);
+            Thread.Sleep(100);
+            Console.WriteLine("ID потока:   " + NumberThread.ManagedThreadId);
+
+
+            //4
+            //2 потока, один чётн, другой-неч
+            Console.WriteLine("2 потока с чётн и нечётн");
+            Console.WriteLine("введите n");
+            n = Convert.ToInt32(Console.ReadLine());
+            Thread threadE, threadO;
+            threadE = new Thread(EvenNumbers);
+            threadE.Priority = ThreadPriority.AboveNormal;//приоритет
+            threadE.Start(n);
+            //threadE.Join();//чётн потом неч
+
+            Console.WriteLine();
+            threadO = new Thread(OddNumbers);
+            threadO.Priority = ThreadPriority.BelowNormal;
+            threadO.Start(n);
+            //threadO.Join();//чередование
+            Console.WriteLine("END");
+
+
+            //5
+            TimerCallback timerCallback = new TimerCallback(WhatTimeIsIt);
+            // позволяет запускать определенные действия по истечению некоторого периода времени:
+            Timer timer = new Timer(timerCallback, null, 500, 1000);       /* null - параметр, которого нет, 500 - время, через которое запустится процесс с таймером, 
+                                                                            * 1000 - периодичность таймера (интервал между вызовами метода делегата). */
+            Thread.Sleep(5000);                                             // 500 - ждем и не закрываем поток
+            timer.Change(Timeout.Infinite, 2000);                           // уничтожение таймера
+
+            void WhatTimeIsIt(object obj)
+            {
+                Console.WriteLine($"It's {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            }
+        }
+    }
+}
---
+++ 3сем/OOP/15/15laba/15laba/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace _15laba
+{
+    class Program
+    {
+        public static BlockingCollection<string> bc;//7TASK
+
+        //1
+        //решето эратосфена
+        public static void First()
+        {
+            int n = 1000;
+            Console.WriteLine("Текущий ID таска: " + Task.CurrentId.ToString());
+            var numbers = new List<uint>();
+            // Заполнение списка numbers числами от 2 до n-1
+            for (var i = 2u; i < n; i++)
+            {
+                numbers.Add(i);
+            }
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                for (var j = 2u; j < n; j++)
+                {
+                    numbers.Remove(numbers[i] * j);
+                }
+            }
+            foreach (int a in numbers)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        //3
+        //3 функции с возвратом значения для вставки значений в 4 функцию
+        public static int Rnd10()
+        {
+            Random rndm = new Random();
+            return rndm.Next(10);
+        }
+        public static int Rnd20()
+        {
+            Random rndm = new Random();
+            return rndm.Next(20);
+        }
+        public static int Rnd30()
+        {
+            Random rndm = new Random();
+            return rndm.Next(30);
+        }
+        //4 функция, использующая резы 3 выше
+        public static int TotalVal(int value1, int value2, int value3)
+        {
+            return (value1 + value2 + value3) / 3;
+        }
+
+        
+        //7
+        public static void AddProduct()
+        {
+            Random r = new Random();
+            int x;
+            List<string> products = new List<string>() { "Ёлка", "Гирлянда", "Шоколадка", "Шампанское", "Мандарины" };
+            for (int i = 0; i < 5; i++)
+            {
+                x = r.Next(0, products.Count - 1);
+                Console.WriteLine("Добавлен товар: " + products[x]);
+                bc.Add(products[x]);
+                products.RemoveAt(x);
+                Thread.Sleep(r.Next(1, 3));
+            }
+            bc.CompleteAdding();
+        }
+        public static void PurchasedProduct()
+        {
+            string str;
+            while (bc.IsAddingCompleted == false)
+            {
+                if (bc.TryTake(out str) == true)
+                    Console.WriteLine("Был куплен товар: " + str);
+                if (bc.TryTake(out str) != true)
+                    Console.WriteLine("Покупатель ушел.");
+            }
+        }
+
+
+        //8
+        //async function
+        public static void ForAsync()
+        {
+            for (int i = 0; i < 30; i++)
+                if (i % 3 == 0)
+                {
+                    Console.Write(i + ", ");
+                    Thread.Sleep(1000);
+                }
+        }
+        public static async void Async()
+        {
+            Console.WriteLine("Асинхронная функция:");
+            await Task.Run(() => ForAsync());
+            Console.WriteLine("Готово!");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //1
+            //поиск простых чисел (желательно взять «решето Эратосфена»)
+            Stopwatch sw = new Stopwatch();//измерение затраченного времени
+            sw.Start();
+            Task task = new Task(() => First());
+            task.Start();
+            Console.WriteLine($"ID: {task.Id}, статус: {task.Status}");
+            task.Wait();
+            Console.WriteLine($"ID: {task.Id}, статус: {task.Status}");
+            sw.Stop();
+            Console.WriteLine($"Производительность: {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine();
+
+
+            //2
+            //та же задача, что в первом, но с токеном отмены
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+            Task task1 = Task.Run(() => First(), cancellation.Token);
+            try
+            {
+                cancellation.Cancel();
+                Console.WriteLine("сработал токен отмены");
+            }
+            catch(Exception)
+            {
+                if (task1.IsCanceled)
+                    Console.WriteLine("ОТМЕНЕНО ERR");
+            }
+
+
+            //3
+            //3 задачи для решения 4ой задачи
+            Task<int> task2 = new Task<int>(Rnd10);
+            Task<int> task3 = new Task<int>(Rnd20);
+            Task<int> task4 = new Task<int>(Rnd30);
+            task2.Start();
+            task3.Start();
+            task4.Start();
+            Task<int> total = new Task<int>(()=>TotalVal(task2.Result,task3.Result,task4.Result));
+            Console.WriteLine("ЗНАЧЕНИЯ: " + task2.Result + ' ' + task3.Result + ' ' + task4.Result);
+            total.Start();
+            Console.WriteLine("РЕЗУЛЬТАТ: " + total.Result);
+
+
+            //4
+            //continuation task
+            Console.WriteLine("\ncontinuation task");
+            //ожидание завершения предшествующих задач
+            Task<int> conTask2 = new Task<int>(Rnd10);
+            Task<int> conTask3 = new Task<int>(Rnd20);
+            Task<int> conTask4 = new Task<int>(Rnd30);
+            Task<int> conTotal = conTask4.ContinueWith((taskN)=>TotalVal(task2.Result, task3.Result, task4.Result));
+            conTask2.Start();
+            conTask3.Start();
+            conTask4.Start();
+            Console.WriteLine("ожидание завершения: "+conTotal.Result);
+            conTask2.Dispose();
+
+            //ожидание
+            Console.WriteLine("Ожидающий");
+            Random rnd = new Random();
+            Task<int> wait = new Task<int>(() => rnd.Next(51));
+            TaskAwaiter<int> awaiter = wait.GetAwaiter();
+            awaiter.OnCompleted(() => Console.WriteLine("Res: "+wait.Result));
+            wait.Start();
+            wait.Wait();
+            awaiter.GetResult();
+            Thread.Sleep(500);
+
+
+            //5
+            //parallel
+            Stopwatch stW = new Stopwatch();
+            int[] arr1 = new int[10000000];
+            int[] arr2 = new int[10000000];
+            Random random = new Random();
+            stW.Restart();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = random.Next(0, 300);
+                arr2[i] = random.Next(0, 300);
+            }
+            stW.Stop();
+            Console.WriteLine("For: " + stW.Elapsed);
+            stW.Restart();
+            Parallel.For(0, arr1.Length, i =>
+            {
+                arr1[i] = random.Next(0, 300);
+                arr2[i] = random.Next(0, 300);
+            });
+            stW.Stop();
+            Console.WriteLine("Параллельный for: " + stW.Elapsed);
+            stW.Restart();
+            Parallel.ForEach<int>(arr1, i =>
+            {
+                arr1[i] = random.Next(0, 300);
+                arr2[i] = random.Next(0, 300);
+            });
+            stW.Stop();
+            Console.WriteLine("Параллельный foreach: " + stW.Elapsed);
+
+
+            //6
+            //parallel.invoke
+            Parallel.Invoke(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Параллельное выполнение блока 1 - " + i);
+                }
+
+                Console.WriteLine("1 готов!");
+            },
+            () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Параллельное выполнение 2 - " + i);
+                }
+
+                Console.WriteLine("2 готов!");
+            });
+
+
+            //7
+            //blockingCollection
+           
+            // Коллекция, которая осуществляет блокировку и ожидает, пока не появится возможность
+            // выполнить действие по добавлению или извлечению элемента
+            bc = new BlockingCollection<string>(5);
+            Task Shop = new Task(AddProduct);
+            Task Clients = new Task(PurchasedProduct);
+            Shop.Start();
+            Clients.Start();
+            Shop.Wait();
+            Clients.Wait();
+
+
+            //8
+            //async await
+            // Tpl – библиотека параллельных задач.
+            // Task –данный класс описывает отдельную задачу, которая запускается асинхронно
+            //в одном из потоков пула потоков.
+            Async();
+            Console.ReadKey();
+
+
+        }
+    }
+}
